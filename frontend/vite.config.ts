944ec037e83f15611b2e4c813f333662495c935b
Guilherme Storti <36459241+guilhermenstorti@users.noreply.github.com>
2026-07-14 14:36:48 -0300

    Configuration for github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/weather-monitor/",
   plugins: [react()],
   test: {
     environment: "jsdom",
